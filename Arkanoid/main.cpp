efd0786382f6185ba21512c401a41697fa3da9b4
andreskballero <acaballerotoledo@gmail.com>
2020-06-25 12:52:38 +0200

    Initial Commit

---
+++ Arkanoid/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Arkanoid
+//
+//  Created by Andrés Caballero Toledo on 25/06/2020.
+//  Copyright © 2020 act. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
